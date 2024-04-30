6cd79eb526e62282e1676a5818746c7535e6473d
briant24 <briantadr24@gmail.com>
2024-05-01 01:38:37 +0700

    website: add background image

--- application/views/auth013/form_login_013.php
+++ application/views/auth013/form_login_013.php
@@ -12,6 +12,8 @@
             font-family: Arial, sans-serif;
             overflow-x: hidden;
             overflow-y: hidden;
+            background-image:  url('<?=base_url('uploads/cats/default.png')?>');
+            background-size : cover;
         }
         .container {
             margin-top: 50px;
@@ -26,7 +28,7 @@
     </style>
 </head>
 <body>
-<section class="vh-100" style="background-color: #508bfc;">
+<section class="vh-100" >
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card shadow-2-strong" style="border-radius: 1rem;">
