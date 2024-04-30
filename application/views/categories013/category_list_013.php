0ba34f5b2f7af407938afe804cef0269ee879777
briant24 <briantadr24@gmail.com>
2024-04-30 22:37:32 +0700

    website: add css

--- application/views/categories013/category_list_013.php
+++ application/views/categories013/category_list_013.php
@@ -5,30 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CATEGORY 013</title>
+    <!-- Sertakan CSS Bootstrap -->
+    <link href="<?=base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
+    <style>
+        .btn-home {
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>CATEGORY 013</h1>
-    <h3>CATEGORY LIST</h3>
-    <a href="<?=base_url()?>"><h4>HOME</h4></a>
-    <hr>
-    <?=$this->session->flashdata('msg')?>
-    <a href="<?=site_url('categories013/add')?>">Add new category</a>
-    <hr>
-    <table border="1">
-        <tr>
-            <th>No</th>
-            <th>Category Name</th>
-            <th>Description</th>
-            <th colspan="2">Action</th>
-        </tr>
-        <?php $i=1; foreach($categories013 as $category) { ?>
-        <tr>
-            <td><?=$i++?></td>
-            <td><?=$category->category_name_013?></td>
-            <td><?=$category->description_013?></td>
-            <td><a href="<?=site_url('categories013/edit/'.$category->category_id_013)?>">Edit</a></td>
-            <td><a href="<?=site_url('categories013/delete/'.$category->category_id_013)?>">Delete</a></td>
-        </tr><?php } ?>
-    </table>
+    <?php $this->load->view('headers'); ?>
+    <div class="container mt-5">
+        <h3>CATEGORY LIST</h3>
+        <hr>
+        <?=$this->session->flashdata('msg')?>
+        <a href="<?=site_url('categories013/add')?>" class="btn btn-success">Add new category</a>
+        <hr>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>No</th>
+                    <th>Category Name</th>
+                    <th>Description</th>
+                    <th colspan="2">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i=1; foreach($categories013 as $category) { ?>
+                <tr>
+                    <td><?=$i++?></td>
+                    <td><?=$category->category_name_013?></td>
+                    <td><?=$category->description_013?></td>
+                    <td><a href="<?=site_url('categories013/edit/'.$category->category_id_013)?>" class="btn btn-info">Edit</a></td>
+                    <td><a href="<?=site_url('categories013/delete/'.$category->category_id_013)?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this category?')">Delete</a></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Sertakan JavaScript Bootstrap -->
+    <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
 </body>
 </html>
